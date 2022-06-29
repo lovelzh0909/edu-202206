135c4c0974b992891eb8f84b1eb853f95a5dba6b
Mr.Zzd <1747672899@qq.com>
2022-06-29 13:19:20 +0000

    创建后端框架

---
+++ Test-back-end/src/main/java/com/example/demo/configure/configure.java
@@ -0,0 +1,12 @@
+package com.example.demo.configure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class configure {
+
+}
---
+++ Test-back-end/src/main/java/com/example/demo/controller/userController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+public class userController {
+    
+}
