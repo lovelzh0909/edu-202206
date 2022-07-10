84c7877fa66efcea06ba793ab6438fffe3fe5c57
ZZD01 <1747672899@qq.com>
2022-07-10 12:45:10 +0800

    功能更新

--- Test-front-end/src/page/Teacherweb/indexTeacher.vue
+++ Test-front-end/src/page/Teacherweb/indexTeacher.vue
@@ -21,61 +21,57 @@ export default {
             items: [
 
                 {
-                    index: 'test2-1',
+                    index: 'test22-1',
                     title: '发布考试',
                     nextItems: [
                         {
-                            index: 'test2-1-1',
+                            index: 'test22-1-1',
                             title: '考试列表'
                         },
                         {
-                            index: 'test2-1-2',
+                            index: 'test22-1-2',
                             title: '发布考试'
                         },
                         {
-                            index: 'test2-1-3',
+                            index: 'test22-1-3',
                             title: '题库'
                         },
                         {
-                            index: 'test2-1-4',
+                            index: 'test22-1-4',
                             title: '试卷库'
                         },
                         {
-                            index: 'test2-1-5',
+                            index: 'test22-1-5',
                             title: '成绩管理'
                         },
                     ]
                 },
                 {
-                    index: 'test2-2',
+                    index: 'test22-2',
                     title: '我的监考',
                     nextItems: [
                         {
-                            index: 'test2-2-1',
+                            index: 'test22-2-1',
                             title: '监考场次'
                         },
                         
                     ]
                 },
                 {
-                    index: 'test2-3',
+                    index: 'test22-3',
                     title: '个人设置',
                     nextItems:[
                         {
-                            index: 'test2-3-1',
+                            index: 'test22-3-1',
                             title: '个人信息'
                         },
                         {
-                            index: 'test2-3-2',
+                            index: 'test22-3-2',
                             title: '修改密码'
                         },
                     ]                    
                 },
                 
-                // {
-                //     index: 'test2-3',
-                //     title: '设置'
-                // },
             ]
         }
     },
