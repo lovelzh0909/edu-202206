696393b741fcee5e292cfa52d8b428df1e385768
heowords <1521552570@qq.com>
2022-07-10 18:43:18 +0800

    remake

---
+++ Test-back-end/src/main/java/com/example/demo/mapper/RuleMapper.java
@@ -0,0 +1,19 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.Rule;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 作者
+ * @since 2022-03-12
+ */
+@Mapper
+public interface RuleMapper extends BaseMapper<Rule> {
+
+}
